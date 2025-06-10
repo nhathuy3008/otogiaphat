e91aa8ebf5b2201ecd9a5a9aa05ae5ee17a5435a
Tran Nhat Huy <nhathuy1237890@gmail.com>
2025-06-10 14:20:57 +0700

    update

--- src/server.js
+++ src/server.js
@@ -122,7 +122,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3001', 'http://localhost:5173','https://fe-otobathanh.vercel.app','https://www.benhvienotogiaphat.com','https://giaphat-front-end.vercel.app'],
+    origin: ['https://benhvienotogiaphat.com', 'http://localhost:5173','https://fe-otobathanh.vercel.app','https://www.benhvienotogiaphat.com','https://giaphat-front-end.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
